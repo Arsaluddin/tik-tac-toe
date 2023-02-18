5b715a36088f16f77831cb63c1591125cfe7281d
arsal uddin <arsaluddin134@gmail.com>
2023-02-18 19:37:45 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+    const [val,setVal] = useState([]);
+    const [flag,setFlag] = useState(true);
+
+    const handleValue = () => {
+        flag ? setVal({...val,val :"x"}) : setVal({...val,val :"O"});
+        flag ? setFlag(false) : setFlag(true);
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div>
+        <div>
+          <button className='btn' onClick={handleValue}>{val}</button>
+          <button className='btn' onClick={handleValue}>{val}</button>
+          <button className='btn' onClick={handleValue}>{val}</button>
+        </div>
+        <div>
+          <button className='btn' onClick={handleValue}>{val}</button>
+          <button className='btn' onClick={handleValue}>{val}</button>
+          <button className='btn' onClick={handleValue}>{val}</button>
+        </div>
+        <div>
+          <button className='btn' onClick={handleValue}>{val}</button>
+          <button className='btn' onClick={handleValue}>{val}</button>
+          <button className='btn' onClick={handleValue}>{val}</button>
+        </div>
+      </div>
+      
     </div>
   );
 }
